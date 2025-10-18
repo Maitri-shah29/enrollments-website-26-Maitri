bfc28dce0e2fdba12e90651976c09fde5d27907e
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-10-19 02:16:01 +0530

    linked login to home page temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import SignIn from "../components/temp-login";
 import Landing from "./components/Landing";
+import SignIn from "./signup/page";
 
 export default function Home() {
   return (
     <>
-      <main>
-        {/* to do real auth */}
-        <SignIn />
-      </main>
       <div className="w-screen h-screen overflow-hidden">
         <Image
           src="/images/backdrop.jpeg"
