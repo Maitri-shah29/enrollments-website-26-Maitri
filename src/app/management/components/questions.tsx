de31bdb0f7c813254a7c07e8ad5030196c3b2732
fabjenny13 <65107441+fabjenny13@users.noreply.github.com>
2025-10-22 17:37:43 +0530

    added components for each button's content

---
+++ src/app/management/components/questions.tsx
@@ -0,0 +1,13 @@
+export default function Round1Questions() {
+  return (
+    <div className="relative bg-white/50 backdrop-blur-md rounded-2xl p-10 w-[90%] h-[90%] shadow-lg overflow-y-auto">
+      <h1 className="text-2xl sm:text-3xl font-bold text-left text-black text-center mb-8">
+        Questions
+      </h1>
+      {/* Dynamic Content */}
+      <div className="text-black leading-relaxed space-y-4 text-base">
+        <p>List of Questions</p>
+      </div>
+    </div>
+  );
+}
