19eb8f6230944fa08a973006beb97faafb01c46a
Arnav Soni <arnavsoni2007@gmail.com>
2025-10-24 23:10:19 +0530

    feat: Added submit button and made it responsive over hovering

--- src/app/clients/cc-client.tsx
+++ src/app/clients/cc-client.tsx
@@ -1,11 +1,13 @@
+import Button from "./components/cc/button";
 import CCNavBar from "./components/cc/navbar";
 import QuestionList from "./components/cc/question-list";
 
 export default function CCClient() {
   return (
     <div className="bg-[#16171B] w-full h-full">
-      <CCNavBar></CCNavBar>
-      <QuestionList></QuestionList>
+      <CCNavBar />
+      <QuestionList />
+      <Button label="Submit" />
     </div>
   );
 }
