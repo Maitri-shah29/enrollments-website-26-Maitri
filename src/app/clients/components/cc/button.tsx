bbdecbef317a76cfcd39a4a506e5284ea0f28144
AdityaVKochar <adityavardhankochar@gmail.com>
2025-10-25 03:00:10 +0530

    fix: fixed button

--- src/app/clients/components/cc/button.tsx
+++ src/app/clients/components/cc/button.tsx
@@ -4,36 +4,32 @@ type ButtonProps = {
 
 function Button({ label }: ButtonProps) {
   return (
-    <div className="flex flex-col h-fit bg-[#16171B] w-full">
-      <div className="mt-auto flex justify-end font-ShareTechMono px-10 py-30">
-        <div className="p-0.2 bg-[#c9eb3e] skew-x-30">
-          <button
-            type="button"
-            className="py-2 px-8 cursor-pointer border-0 outline-none"
-            style={{
-              backgroundColor: "#16171b",
-              color: "#c9eb3e",
-              transition: "background 0.2s, color 0.2s",
-              border: "2px solid #c9eb3e",
-            }}
-            onMouseEnter={(e) => {
-              e.currentTarget.style.backgroundColor = "#c9eb3e";
-              e.currentTarget.style.color = "#16171b";
-            }}
-            onMouseLeave={(e) => {
-              e.currentTarget.style.backgroundColor = "#16171b";
-              e.currentTarget.style.color = "#c9eb3e";
-            }}
-          >
-            <span
-              className="inline-block -skew-x-30"
-              style={{ fontFamily: "var(--font-share-tech)" }}
-            >
-              {label}
-            </span>
-          </button>
-        </div>
-      </div>
+    <div className="p-0.2 bg-[#c9eb3e] skew-x-30 w-min">
+      <button
+        type="button"
+        className="py-2 px-8 cursor-pointer border-0 outline-none font-ShareTechMono"
+        style={{
+          backgroundColor: "#16171b",
+          color: "#c9eb3e",
+          transition: "background 0.2s, color 0.2s",
+          border: "2px solid #c9eb3e",
+        }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.backgroundColor = "#c9eb3e";
+          e.currentTarget.style.color = "#16171b";
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.backgroundColor = "#16171b";
+          e.currentTarget.style.color = "#c9eb3e";
+        }}
+      >
+        <span
+          className="inline-block -skew-x-30"
+          style={{ fontFamily: "var(--font-share-tech)" }}
+        >
+          {label}
+        </span>
+      </button>
     </div>
   );
 }
