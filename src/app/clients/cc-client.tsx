b0f92cc58fd0130f1eb661d1e2eae49aca022eca
AdityaVKochar <adityavardhankochar@gmail.com>
2025-10-25 02:58:16 +0530

    feat: added answer box for cc

--- src/app/clients/cc-client.tsx
+++ src/app/clients/cc-client.tsx
@@ -1,3 +1,4 @@
+import AnswerBox from "./components/cc/answer-box";
 import Button from "./components/cc/button";
 import CCNavBar from "./components/cc/navbar";
 import QuestionList from "./components/cc/question-list";
@@ -7,6 +8,7 @@ export default function CCClient() {
     <div className="bg-[#16171B] w-full h-full">
       <CCNavBar />
       <QuestionList />
+      <AnswerBox subject="Question" body=""></AnswerBox>
       <Button label="Submit" />
     </div>
   );
