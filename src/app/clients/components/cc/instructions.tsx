44eacf6eaa3026ade90491c7d47e21a007f646d6
MKDev121 <meatk121@gmail.com>
2025-10-27 21:46:17 +0530

    Created a seperate script of  NeonSection componenet

--- src/app/clients/components/cc/instructions.tsx
+++ src/app/clients/components/cc/instructions.tsx
@@ -1,19 +1,6 @@
 import Image from "next/image";
 import type React from "react";
-
-const NeonSection: React.FC<{
-  title: string;
-  children: React.ReactNode;
-}> = ({ title, children }) => (
-  <div className="w-full border-4 border-[#242527] bg-transparent">
-    <div className="border-b-4 border-[#242527] bg-[#242527] px-4 py-2 text-[#C9EB3E] font-ShareTechMono text-lg">
-      {title}
-    </div>
-    <div className="px-5 py-6 text-white leading-relaxed tracking-wide bg-[#16171B] font-ShareTechMono">
-      {children}
-    </div>
-  </div>
-);
+import NeonSection from "./neon-section";
 
 const Instructions: React.FC = () => {
   return (
---
+++ src/app/clients/components/cc/neon-section.tsx
@@ -0,0 +1,15 @@
+const NeonSection: React.FC<{
+  title: string;
+  children: React.ReactNode;
+}> = ({ title, children }) => (
+  <div className="w-full border-4 border-[#242527] bg-transparent">
+    <div className="border-b-4 border-[#242527] bg-[#242527] px-4 py-2 text-[#C9EB3E] font-ShareTechMono text-lg">
+      {title}
+    </div>
+    <div className="px-5 py-6 text-white leading-relaxed tracking-wide bg-[#16171B] font-ShareTechMono space-y-10">
+      {children}
+    </div>
+  </div>
+);
+
+export default NeonSection;
