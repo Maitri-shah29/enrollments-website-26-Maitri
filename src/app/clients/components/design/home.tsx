6f71183cb35041b413883cea9317b393915d639b
POSTI-25 <jahnavisingh512@gmail.com>
2025-10-28 14:03:58 +0530

    design default home page

---
+++ src/app/clients/components/design/home.tsx
@@ -0,0 +1,12 @@
+"use client";
+import type React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="border border-black m-4 p-10 flex items-center justify-center text-xl">
+      Welcome to ACM VIT Design
+    </div>
+  );
+};
+
+export default Home;
