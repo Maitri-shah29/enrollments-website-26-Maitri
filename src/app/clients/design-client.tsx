d84f8d83adc5c7f092a4b6e609d67947d039fb60
POSTI-25 <jahnavisingh512@gmail.com>
2025-10-28 14:15:11 +0530

    design client page connecting all

---
+++ src/app/clients/design-client.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+import About from "./components/design/about";
+import AOIs from "./components/design/aoi";
+import DesignNavbar from "./components/design/design-navbar";
+import Home from "./components/design/home";
+import Instructions from "./components/design/instructions";
+import Interview from "./components/design/interview";
+import Questions from "./components/design/questions";
+
+const DesignClient = () => {
+  const [selectedPanel, setSelectedPanel] = useState<string>("Home");
+
+  return (
+    <div className="flex flex-col w-full h-screen border border-black">
+      <DesignNavbar selected={selectedPanel} onSelect={setSelectedPanel} />
+      <div className="flex-grow overflow-auto">
+        {selectedPanel === "Home" && <Home />}
+        {selectedPanel === "About" && <About />}
+        {selectedPanel === "Instructions" && <Instructions />}
+        {selectedPanel === "AOIs" && <AOIs />}
+        {selectedPanel === "Questions" && <Questions />}
+        {selectedPanel === "Interview" && <Interview />}
+      </div>
+    </div>
+  );
+};
+
+export default DesignClient;
