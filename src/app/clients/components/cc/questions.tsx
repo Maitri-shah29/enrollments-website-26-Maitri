5f86003fa4750dfaf4ab71015cf411190788f893
Samarth Naik <naikm.sam@gmail.com>
2025-11-05 21:50:35 +0530

    fixed issue

--- src/app/clients/components/cc/questions.tsx
+++ src/app/clients/components/cc/questions.tsx
@@ -263,7 +263,6 @@ const Questions = ({ roundUser: initialRoundUser }: QuestionsProps) => {
                   <Button
                     label={submitting ? "Submitting..." : "Submit"}
                     onClick={handleSubmit}
-                    disabled={submitting}
                   />
                 </div>
               </div>
