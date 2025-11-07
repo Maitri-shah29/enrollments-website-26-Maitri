adfac27d9d001325c58f3851522d5e01c25d8a9f
AdityaVKochar <adityavardhankochar@gmail.com>
2025-11-08 00:14:10 +0530

    fix: redisgn of mobile ui

--- src/app/components/mobile-blocker.tsx
+++ src/app/components/mobile-blocker.tsx
@@ -1,22 +1,34 @@
 export default function MobileBlocker() {
   return (
     <div
-      className="font-doppio md:hidden fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-[#031927] text-white p-6 text-center space-y-10"
-      role="alert"
-      aria-live="assertive"
+      className="font-doppio md:hidden fixed inset-0 z-[9999]"
+      style={{
+        backgroundImage: "url('/images/acm-guy.png')",
+        backgroundSize: "auto 80%",
+        backgroundPosition: "left bottom",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "#000",
+      }}
     >
-      <img
-        src="images/acm-mobile-logo.svg"
-        alt="ACM Mobile logo"
-        className="w-70 h-auto mx-auto"
-      />
-
-      <h1 className="text-xl">
-        Are you really trying to apply to a tech chapter from your phone?
-      </h1>
-      <p className="text-md">
-        Come back here from your laptop and keep the screen maximized!
-      </p>
+      <div className="absolute inset-0 bg-[#000]/40" />
+      <div
+        className="relative z-10 flex h-full flex-col items-center justify-center text-white p-6 text-center space-y-10"
+        role="alert"
+        aria-live="assertive"
+      >
+        <div
+          className="text-white text-center font-extrabold leading-normal font-poppins text-[37.056px] custom-text-shadow"
+          style={{ textShadow: "0 3px 1.705px #838384" }}
+        >
+          ACM - VIT
+        </div>
+        <h1 className="text-xl">
+          Are you really trying to apply to a tech chapter from your phone?
+        </h1>
+        <p className="text-md">
+          Come back here from your laptop and keep the screen maximized!
+        </p>
+      </div>
     </div>
   );
 }
