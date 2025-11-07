dca4cec692d885975271375dc55b8dfe5663add4
ludicrouslytrue <ludicrouslytrue@gmail.com>
2025-11-07 23:45:31 +0530

    minor ui changes on management  page, see issue #227 for list

--- src/app/clients/management-client.tsx
+++ src/app/clients/management-client.tsx
@@ -331,12 +331,12 @@ export default function Management() {
 
       {/* Main Content */}
       <main className="flex-1 flex flex-col justify-center items-center px-8">
-        <div className="w-full flex justify-start h-[8%] ml-[10%] ">
-          <div className="p-2 gap-2 flex flex-row bg-white/40 w-[50%] rounded-full mt-2">
+        <div className="w-full flex justify-center h-[8%]">
+          <div className="p-2 gap-2 flex flex-row items-center bg-white/40 w-[50%] rounded-full mt-2">
             <Search />
             <input
               type="text"
-              placeholder="Search Domain"
+              placeholder="Search"
               className="outline-none flex-1 text-white placeholder-white-500"
             />
           </div>
