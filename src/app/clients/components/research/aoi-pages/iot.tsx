50adab1cdf99faed008da6af4effe622f0befbc1
Aayush <aayush.dwivedi2024@vitstudent.ac.in>
2025-11-09 01:14:49 +0530

    fix:aoi pages fixed for smaller screens

--- src/app/clients/components/research/aoi-pages/iot.tsx
+++ src/app/clients/components/research/aoi-pages/iot.tsx
@@ -6,25 +6,29 @@ import NetworkGraph from "../network-graph";
 
 const iotPage: React.FC = () => {
   return (
-    <div className="relative flex w-full h-full bg-[#1A1A1A]">
+    <div className="relative flex w-full h-full bg-[#1A1A1A] overflow-hidden">
       <NetworkGraph />
 
-      <div className="absolute top-0 p-10 z-10">
-        <h1 className="text-2xl font-bold text-[#C8B7FF] mb-10">IOT</h1>
-        <p className="text-white">
-          lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          lorem ipsum lorem ipsum lorem ipsum v lorem ipsum eugfuw kurfuwbfhweb
-          rbfuyrgp4ugfhfiuwe psiuhfouwgfiewb bwiubfiuwgfwegf7gw uwebcuhsdbvouwg8
-          wegf87gf wegfo8wegf9w7ef
-          wbcusbdvy8gwefesdbjckusbdvywegvwbvhbvjsbvduv8wgvbvhjbvjhsdv sd
-          yugrvehvbwehbvhewbvhsdbvbvyweguewbewbvhbvyubvuywegvubhvbvweyfwe8hfbf
-          fugo8wegvobv bvuwewe eubo8wegowyebvuybewuybvuvb kjhbv wbefiyug
-          djkfvbkuebvuyre kjfbviyuerbi8yr i'll kill myself wkjehbiwfyw
-          kwhbvwuyvb8wey khvbygrv8owgvo8wegfw wuebo8wegfo8we7gew
-          uvbowvogwe789fwe jhvbouwgvo8gwvo8vo8 kfubhvouego8rgv
-          huvboygv8gf87wef87wefrvb ubvygvo8gfo8we7
-        </p>
+      <div className="absolute top-0 left-0 right-0 p-4 sm:p-6 md:p-8 lg:p-10 z-10 max-w-4xl">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-[#C8B7FF] mb-4 sm:mb-6 md:mb-10">
+          IoT
+        </h1>
+        <div className="max-h-[calc(100vh-120px)] overflow-y-auto pr-2 sm:pr-4">
+          <p className="text-white text-sm sm:text-base md:text-lg leading-relaxed">
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+            ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+            lorem ipsum lorem ipsum lorem ipsum v lorem ipsum eugfuw
+            kurfuwbfhweb rbfuyrgp4ugfhfiuwe psiuhfouwgfiewb bwiubfiuwgfwegf7gw
+            uwebcuhsdbvouwg8 wegf87gf wegfo8wegf9w7ef
+            wbcusbdvy8gwefesdbjckusbdvywegvwbvhbvjsbvduv8wgvbvhjbvjhsdv sd
+            yugrvehvbwehbvhewbvhsdbvbvyweguewbewbvhbvyubvuywegvubhvbvweyfwe8hfbf
+            fugo8wegvobv bvuwewe eubo8wegowyebvuybewuybvuvb kjhbv wbefiyug
+            djkfvbkuebvuyre kjfbviyuerbi8yr i'll kill myself wkjehbiwfyw
+            kwhbvwuyvb8wey khvbygrv8owgvo8wegfw wuebo8wegfo8we7gew
+            uvbowvogwe789fwe jhvbouwgvo8gwvo8vo8 kfubhvouego8rgv
+            huvboygv8gf87wef87wefrvb ubvygvo8gfo8we7
+          </p>
+        </div>
       </div>
     </div>
   );
