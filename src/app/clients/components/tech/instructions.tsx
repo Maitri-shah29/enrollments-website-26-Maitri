8f58805a3a1b3b78f28638ff8bb8a1d8cca031ca
Ishitajoshii <ishitata.joshi@gmail.com>
2025-12-12 22:27:23 +0530

    instructions added

--- src/app/clients/components/tech/instructions.tsx
+++ src/app/clients/components/tech/instructions.tsx
@@ -13,13 +13,21 @@ export default function Instructions() {
         />
       </div>
       <div className="flex text-[#993C7A] font-mono text-lg leading-relaxed mt-15">
-        <pre className="text-right pr-4 select-none text-[#993C7A]">
+        {/* <pre className="text-right pr-4 select-none text-[#993C7A]">
           {Array.from({ length: 6 }, (_, i) => (
             <div key={`instructions-line-${i + 1}`}>{i + 1}</div>
           ))}
-        </pre>
+        </pre> */}
         <pre className="whitespace-pre-wrap text-[#E097CE]">
-          {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel nisi at nisl luctus tincidunt. Aliquam semper erat et nibh scelerisque vulputate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel nisi at nisl luctus tincidunt. Aliquam semper erat et nibh scelerisque vulputate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel nisi at nisl luctus tincidunt. Aliquam semper erat et nibh scelerisque vulputate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel nisi at nisl luctus tincidunt. Aliquam semper erat et nibh scelerisque vulputate.`}
+          {`1. Go to the explore tab on the left sidebar and join upto 2 areas of interest according to your preferences.
+2. Proceed to Round 1 from the left sidebar
+3. Answer the common questions first, make sure to click on save after every answer.
+4. If you see the "saved successfully" popup on the top right, your answer has been saved.
+5. Answer the domain-wise questions next, ensure that you click on save after every answer.
+6. After answering all questions, click on submit form at the bottom of the left sidebar.
+7. You will be judged on your technical skills but more importantly on your ability to quickly learn new concepts. Points will be given for originality.
+
+All the Best!`}
         </pre>
       </div>
     </div>
