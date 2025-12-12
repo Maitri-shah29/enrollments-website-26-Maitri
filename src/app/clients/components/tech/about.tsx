565d0fc81f0f84d182777a9cf90adb2456f3aad4
ludicrouslytrue <ludicrouslytrue@gmail.com>
2025-12-12 22:50:28 +0530

    content

--- src/app/clients/components/tech/about.tsx
+++ src/app/clients/components/tech/about.tsx
@@ -14,9 +14,7 @@ export default function About() {
       </div>
       <div className="flex text-[#993C7A] font-mono text-lg leading-relaxed mt-15">
         <pre className="text-[#E097CE] whitespace-pre-wrap max-w-4xl">
-          {`What's a tech chapter without its tech spirit?
-At ACM VIT, the Tech Domain brings together creators from Web, App, DevOps, FOSS, and GameDev to build things that actually ship. From clean architecture and testing to performance, accessibility, and security, we focus on engineering that matters.
-We love building cool, impactful projects, contributing to open source, and learning by building — together.`}
+          {`The Tech Domain of ACM VIT focuses on exploring and developing practical technical skills through projects, workshops, and collaboration. It covers areas such as web and app development, AI, and other emerging technologies. Members gain hands-on experience while learning to build real-world solutions and contribute to open-source initiatives. The domain fosters an environment where ideas evolve into impactful technology and learning happens through application.`}
         </pre>
       </div>
     </div>
