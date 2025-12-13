c20d2f00d3605cc956ba477ed353797860e6c208
Aayush <aayush.dwivedi2024@vitstudent.ac.in>
2025-12-13 19:29:34 +0530

    pls work

--- src/app/components/landing/home-page.tsx
+++ src/app/components/landing/home-page.tsx
@@ -135,7 +135,10 @@ const HomePage: React.FC<HomePageProps> = ({ onNavigateKeyword }) => {
       </div>
 
       <div className="relative mx-auto flex h-full w-full max-w-[90vw] md:max-w-[92vw] xl:max-w-[94vw] 2xl:max-w-[96vw] flex-col gap-8 px-4 pt-8 sm:px-6 lg:px-10 2xl:px-12">
-        <div className="grid w-full gap-3 sm:grid-cols-[auto_minmax(0,1fr)_auto] sm:items-center sm:gap-5">
+        <div
+          className="grid w-full gap-3 sm:grid-cols-[auto_minmax(0,1fr)_auto] sm:items-center sm:gap-5"
+          onClick={handleKeyword("acmvit.in")}
+        >
           <div className="relative h-26 w-44 justify-self-start sm:h-20 sm:w-56">
             <Image
               src="/images/acmlogo.svg"
@@ -158,10 +161,7 @@ const HomePage: React.FC<HomePageProps> = ({ onNavigateKeyword }) => {
         </div>
 
         <div className="grid gap-4 2xl:gap-5 auto-rows-[22vh] sm:auto-rows-[24vh] lg:auto-rows-[26vh] xl:auto-rows-[28vh] 2xl:auto-rows-[30vh] grid-cols-12 h-full">
-          <div
-            className="col-span-6 relative"
-            onClick={handleKeyword("acmvit.in")}
-          >
+          <div className="col-span-6 relative" onClick={handleKeyword("about")}>
             <div className="w-full h-full bg-[#292625] rounded-xl flex items-center text-5xl font-poppins pl-10">
               <h1>About ACM</h1>
               <Image
