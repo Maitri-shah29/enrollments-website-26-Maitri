3a1047b6cbab5b4bc9c60c41f4ed600ce770f29d
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-12-17 02:30:09 +0530

    LITERALLY ITNA DUMB MISTAKE OMG

--- src/app/components/landing/tab-constants.ts
+++ src/app/components/landing/tab-constants.ts
@@ -176,7 +176,7 @@ export const titleFromDomain = (value: string) => {
 
 export const BOOKMARK_LABELS: Record<string, string> = {
   instructions: "Instructions",
-  cc: "Competitive Coding",
+  cc: "CC",
   management: "Management",
   tech: "Tech",
   design: "Design",
