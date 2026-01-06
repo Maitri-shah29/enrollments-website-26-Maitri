63b7981f869415c6678b3ed75449125ccb01d514
Ashman Singh <36335693+thinkter@users.noreply.github.com>
2026-01-06 17:24:34 +0530

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -30,7 +30,7 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      // hd: "vitstudent.ac.in",
+      hd: "vitstudent.ac.in",
       accessType: "offline",
       prompt: "select_account consent",
     },
