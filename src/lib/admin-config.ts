1c5893e21c920f99cd7d7569c1779d7af42d0463
Technical Director <87973905+technical-director-acmvit@users.noreply.github.com>
2026-01-14 19:02:17 +0400

    Update admin-config.ts

--- src/lib/admin-config.ts
+++ src/lib/admin-config.ts
@@ -5,7 +5,6 @@
 
 export const ADMIN_EMAILS = [
   // Admin emails allowed to sign in (deduplicated)
-  "ashman.singh2024@vitstudent.ac.in",
   "maitri.shah20242024@vitstudent.ac.in",
   "navaneeth.krishna2024@vitstudent.ac.in",
   "sree.rajmuthaiyaal2024@vitstudent.ac.in",
